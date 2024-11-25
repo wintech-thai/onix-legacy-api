881a8069223edddc221941ab24b43cf8f75c1770
pjames <pjames@gmail.com>
2024-11-25 21:32:59 +0700

    Added PAYROLL_NOTE

--- lib_wis_erp_framework/frameworks/models/MPayrollDocumentItem.php
+++ lib_wis_erp_framework/frameworks/models/MPayrollDocumentItem.php
@@ -47,6 +47,7 @@ class MPayrollDocumentItem extends MBaseModel
                     'BAL_YEAR_DEFINITION:S:BAL_YEAR_DEFINITION:N',
                     'ENDING_TOTAL_DEFINITION:S:ENDING_TOTAL_DEFINITION:N',
                     'ENDING_YEAR_DEFINITION:S:ENDING_YEAR_DEFINITION:N',
+                    'PAYROLL_NOTE:S:PAYROLL_NOTE:N',
 
                     'CREATE_DATE:CD:CREATE_DATE:N',
                     'MODIFY_DATE:MD:MODIFY_DATE:N',
@@ -89,6 +90,7 @@ class MPayrollDocumentItem extends MBaseModel
                     'PDI.BAL_YEAR_DEFINITION:S:BAL_YEAR_DEFINITION:N',
                     'PDI.ENDING_TOTAL_DEFINITION:S:ENDING_TOTAL_DEFINITION:N',
                     'PDI.ENDING_YEAR_DEFINITION:S:ENDING_YEAR_DEFINITION:N',
+                    'PDI.PAYROLL_NOTE:S:PAYROLL_NOTE:N',
 
                     'EM.EMPLOYEE_CODE:S:EMPLOYEE_CODE:N',
                     'EM.EMPLOYEE_NAME:S:EMPLOYEE_NAME:N',
